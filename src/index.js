d0202d58283dae106c48e9f0c51023807dfa1362
Choco <trust2065@gmail.com>
2019-09-01 18:46:18 +1000

    image modal

--- src/index.js
+++ src/index.js
@@ -14,23 +14,37 @@ window.addEventListener("click", e => {
   if (e.target.localName === "a") {
     console.log("target: ", e.target);
   }
+  // modal
+  if (
+    e.target.localName === "img" &&
+    e.target.classList.contains("sectionImage")
+  ) {
+    document.querySelector("#imgModal .modal-body").src = e.target.src;
+    document.querySelector("#imgModal .modal-body").alt = e.target.alt;
+    document.querySelector("#imgModal").style.display = "block";
+  } else if (e.target.getAttribute("data-dismiss")) {
+    document.querySelector(
+      `#${e.target.getAttribute("data-dismiss")}`
+    ).style.display = "none";
+  }
 });
 
 // open modal when click image
-const modal = document.querySelector("#imageModal");
-const modalImage = document.querySelector("#imageModal img");
-document.querySelectorAll("#section1 .imageBox__image").forEach(e => {
-  e.addEventListener("click", e => {
-    modalImage.src = e.target.src;
-    modalImage.alt = e.target.alt;
-    modal.classList.add("modal--flex");
-  });
-});
+
+// const modal = document.querySelector("#imageModal");
+// const modalImage = document.querySelector("#imageModal img");
+// document.querySelectorAll("#section1 .imageBox__image").forEach(e => {
+//   e.addEventListener("click", e => {
+//     modalImage.src = e.target.src;
+//     modalImage.alt = e.target.alt;
+//     modal.classList.add("modal--flex");
+//   });
+// });
 
 // close modal when click X
-document.querySelector(".modal__close").addEventListener("click", () => {
-  modal.classList.remove("modal--flex");
-});
+// document.querySelector(".modal__close").addEventListener("click", () => {
+//   modal.classList.remove("modal--flex");
+// });
 
 // close modal when click out of modal area
 // window.onclick = e => {
